b6b680af3aca2e35fe9d96d1398d7a7f8c620f64
Caio Renato BCarmo <caio.bedulli@gmail.com>
2020-03-30 01:06:53 +0200

    Add get_from_env_or_settings method

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,13 @@
+import os
+
 from pyramid.config import Configurator
+
+
+def get_from_env_or_settings(settings_name, settings, env_name=None, default=None):
+    if not env_name:
+        env_name = settings_name.upper().replace('.', '_')
+
+    return os.environ.get(env_name, settings.get(settings_name, default))
 
 
 def main(global_config, **settings):
